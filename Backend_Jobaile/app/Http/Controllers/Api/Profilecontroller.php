a12cfa80934b944b921798f5ebd38dfcd0c57f96
FranklinJaya2006 <148520978+FranklinJaya2006@users.noreply.github.com>
2025-05-17 23:33:31 +0800

    profile

--- Backend_Jobaile/app/Http/Controllers/Api/Profilecontroller.php
+++ Backend_Jobaile/app/Http/Controllers/Api/Profilecontroller.php
@@ -7,5 +7,38 @@ use Illuminate\Http\Request;
 
 class Profilecontroller extends Controller
 {
-    //
+    public function profileuser(Request $request)
+    {
+        try {
+            $user = auth()->user();
+
+            if (!$user) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'User not found',
+                ], 404);
+            }
+
+            $userProfile = [
+                'fullname' => $user->fullname,
+                'email' => $user->email,
+                'phone' => $user->phone,
+                'ktp_card_path' => $user->ktp_card_path
+            ];
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User profile fetched successfully',
+                'data' => $userProfile,
+            ], 200);
+            
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'An error occurred while fetching user profile',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+
+    }
 }
--- Backend_Jobaile/routes/api.php
+++ Backend_Jobaile/routes/api.php
@@ -3,11 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\Profilecontroller;
 
 Route::post('/register', [AuthController::class, 'register']); // bisa
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/profile', [Profilecontroller::class, 'profileuser']); // bisa
 });
 
